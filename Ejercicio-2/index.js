b28db87fcdd29172e45297eff55a53fc481544af
brayanmezac <brayanmezac.dev@gmail.com>
2022-12-06 19:30:12 -0500

    👅

---
+++ Ejercicio-2/index.js
@@ -0,0 +1,12 @@
+const list = [
+    "brayan meza",
+    23,
+    true,
+    new Date(1999, 04, 10),
+    book_fav = {
+      tittle: "satanas",
+      autor: "mario mendoza",
+      dataBook: new Date(2002, 02, 06),
+      url: "https://en.wikipedia.org/wiki/Satan%C3%A1s_(novel)"
+    }
+  ]
